6f027ecd03981d7c5aa441a4a273265f17135f84
Augusto Seabra <augusto.seabra00@gmail.com>
2022-05-31 18:44:56 -0300

    Adicionando v1

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const char = document.querySelector("[data-value='char']")
+const obstacle = document.querySelector("[data-value='obstacle']")
+const scenery = document.querySelector("[data-value='scenery']")
+
+const charAnimationDuration = window.getComputedStyle(document.documentElement).getPropertyValue("--char-animation-duration") ? window.getComputedStyle(document.documentElement).getPropertyValue("--char-animation-duration") : 500
+const charWidth = window.getComputedStyle(document.documentElement).getPropertyValue("--char-width") ? +window.getComputedStyle(document.documentElement).getPropertyValue("--char-width").replace("px", "") : 120
+
+const obstacleWidth = window.getComputedStyle(document.documentElement).getPropertyValue("--obstacle-width") ? +window.getComputedStyle(document.documentElement).getPropertyValue("--obstacle-width").replace("px", "") : 80
+
+const charDeadWidth = window.getComputedStyle(document.documentElement).getPropertyValue("--char-dead-width") ? window.getComputedStyle(document.documentElement).getPropertyValue("--char-dead-width") : "65px"
+const charDeadMarginLeft = window.getComputedStyle(document.documentElement).getPropertyValue("--char-dead-margin-left") ? window.getComputedStyle(document.documentElement).getPropertyValue("--char-dead-margin-left") : "55px"
+const charDeadSrc = "./images/game-over.webp"
+
+const handleJump = event => {
+  try {
+    if(!char) return window.location.reload()
+    const { keyCode } = event
+    const validKeyCodes = [37, 38, 39, 40, 32]
+    if (!validKeyCodes.includes(keyCode)) return false
+    char.classList.add("char-jump")
+    setTimeout(() => {
+      char.classList.remove("char-jump")
+    }, charAnimationDuration)
+  }catch(e) {
+    console.error("Erro ao tentar pular: ", e)
+  }
+}
+
+window.addEventListener('keydown', handleJump)
+
+const gameLoop = setInterval(() => {
+  try {
+    const obstaclePosition = obstacle.offsetLeft
+    const charPosition = +window.getComputedStyle(char).getPropertyValue("bottom").replace("px", "")
+    const sceneryPosition = scenery.offsetLeft
+
+    if(obstaclePosition <= charWidth && obstaclePosition > 0 && charPosition < obstacleWidth) {
+      obstacle.style.animation = "none"
+      obstacle.style.left = `${obstaclePosition}px`
+      
+      char.style.animation = "none"
+      char.src = charDeadSrc
+      char.style.bottom = `${charPosition}px`
+      char.style.width = charDeadWidth
+      char.style.marginLeft = charDeadMarginLeft
+
+      scenery.style.animation = "none"
+      scenery.style.left = `${sceneryPosition}px`
+
+      clearInterval(gameLoop)
+      window.removeEventListener('keydown', handleJump)
+    }
+  }catch(e) {
+    console.error("Erro ao tentar atualizar obstáculo: ", e)
+    clearInterval(gameLoop)
+  }
+}, 10)
